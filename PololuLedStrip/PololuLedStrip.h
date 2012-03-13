d6e4dd3a3292094dd788b17aa0f019b6efffc606
David (Pololu) <dev-david@pololu.com>
2012-03-12 20:08:48 -0700

    Fixed the timing for 20 MHz processors.

--- PololuLedStrip/PololuLedStrip.h
+++ PololuLedStrip/PololuLedStrip.h
@@ -120,10 +120,13 @@ namespace Pololu
         "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
         "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
 #endif
-        "brcc .+2\n" "cbi %2, %3\n"              // If the bit to send is 1, drive the line low now.
+        "brcc .+2\n" "cbi %2, %3\n"              // If the bit to send is 1, drive the line low now.	
+#if F_CPU == 16000000
         "nop\n"
+#elif F_CPU == 20000000
+        "nop\n" "nop\n" "nop\n" "nop\n"
+#endif
         "ret\n"
-        
         "led_strip_asm_end%=: "
         : "=b" (colors)
         : "0" (colors),         // %a0 points to the next color to display
