5bd02e23d2fd3c941955b83be7baec2151d3cd03
David (Pololu) <dev-david@pololu.com>
2013-10-07 17:48:19 -0700

    Updated the 16 MHz side to work with the high-speed protocol.  It as not as fast as it could theoretically be; it takes about 1.1 ms to update 30 LEDs.

--- PololuLedStrip/PololuLedStrip.h
+++ PololuLedStrip/PololuLedStrip.h
@@ -280,8 +280,6 @@ namespace Pololu
 
 #if F_CPU == 16000000
         "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
-        "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
-        "nop\n" "nop\n" "nop\n"
 #elif F_CPU == 20000000
         "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
         "nop\n" "nop\n"
@@ -290,8 +288,6 @@ namespace Pololu
         "brcc .+2\n" "cbi %2, %3\n"              // If the bit to send is 1, drive the line low now.
 
 #if F_CPU == 16000000
-        "nop\n" "nop\n" "nop\n" "nop\n" "nop\n"
-        "nop\n" "nop\n" "nop\n"
 #elif F_CPU == 20000000
 #endif
 
