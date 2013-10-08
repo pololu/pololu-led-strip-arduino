d10cf4013ee7a4e3ae1e14b49a1f110b5d57a8e8
David (Pololu) <dev-david@pololu.com>
2013-10-07 17:49:21 -0700

    Removed some un-needed ifdefs.

--- PololuLedStrip/PololuLedStrip.h
+++ PololuLedStrip/PololuLedStrip.h
@@ -287,10 +287,6 @@ namespace Pololu
 
         "brcc .+2\n" "cbi %2, %3\n"              // If the bit to send is 1, drive the line low now.
 
-#if F_CPU == 16000000
-#elif F_CPU == 20000000
-#endif
-
         "ret\n"
         "led_strip_asm_end%=: "
         : "=b" (colors)
