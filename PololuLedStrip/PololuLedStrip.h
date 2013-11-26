b02f4f3d03c91b7b556f53f735897044c8e18ca1
David (Pololu) <dev-david@pololu.com>
2013-11-26 11:56:11 -0800

    Changed the reset delay from 24 microseconds to 50 microseconds because the WS2812B datasheet specifies 50 microseconds (though experimentally it seems like 6 microseconds would be enough).

--- PololuLedStrip/PololuLedStrip.h
+++ PololuLedStrip/PololuLedStrip.h
@@ -360,7 +360,7 @@ namespace Pololu
       }
     }
     __enable_irq();         // Re-enable interrupts now that we are done.
-    delayMicroseconds(24);  // Hold the line low for 24 microseconds to send the reset signal.
+    delayMicroseconds(50);  // Hold the line low for 50 microseconds to send the reset signal.
   }
 
 }
